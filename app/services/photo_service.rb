1c8a6e9bd25f98be5baa8e7cddec665ca77adaa9
Margo Flewelling <flewelling.margo@gmail.com>
2020-06-05 23:08:55 -0600

    Add photo service calling to the teleport api for an image

---
+++ app/services/photo_service.rb
@@ -0,0 +1,11 @@
+class PhotoService
+
+  def photo(location)
+    city = location.split(',').first
+    conn = Faraday.get("https://api.teleport.org/api/urban_areas/slug:#{city}/images/")
+    json = JSON.parse(conn.body, symbolize_names: true)
+    img = json[:photos].first[:image][:mobile]
+    img
+  end
+
+end
