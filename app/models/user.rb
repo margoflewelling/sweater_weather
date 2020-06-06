048e35b3423b2ee66cca68ad66fb4115ced51842
Margo Flewelling <flewelling.margo@gmail.com>
2020-06-06 15:16:54 -0600

    Add method for generating api key

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
+  before_create :set_api_key
   validates :email, uniqueness: { case_sensitive: false }
   has_secure_password
+
+  def set_api_key
+    self.api_key = SecureRandom.base58(24)
+  end
 
 end
