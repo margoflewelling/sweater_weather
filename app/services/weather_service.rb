e68f1cd513a73addde9e87d3f6e7873b6b75c34c
Margo Flewelling <flewelling.margo@gmail.com>
2020-06-05 20:56:06 -0600

    User coordinates to call to openWeatherApi

---
+++ app/services/weather_service.rb
@@ -0,0 +1,12 @@
+class WeatherService
+
+  def weather(coordinates)
+    weather = Faraday.get("https://api.openweathermap.org/data/2.5/onecall") do |f|
+      f.params['lat'] = coordinates[:lat]
+      f.params['lon'] = coordinates[:lng]
+      f.params['exclude'] = 'minutely'
+      f.params['appid'] = ENV['WEATHER_KEY']
+    end
+    JSON.parse(weather.body, symbolize_names: true)
+  end
+end
