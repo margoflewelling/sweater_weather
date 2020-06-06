63c66c901698dbd674353803e1004afb1f628daa
Margo Flewelling <flewelling.margo@gmail.com>
2020-06-06 15:17:20 -0600

    User model testing validations and generating key

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_uniqueness_of(:email).case_insensitive}
+  end
+
+  describe 'set_api_key' do
+    it 'can generate an api key' do
+      user = User.create(email: "margo@example.com", password: "123", password_confirmation: "123")
+      expect(user.api_key).to be_a String
+      expect(user.api_key.length).to eq(24)
+    end
+  end
+
 end
