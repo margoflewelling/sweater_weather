1cd422165e28bd8164acdad511ee3a08ae9d8ba7
Margo Flewelling <flewelling.margo@gmail.com>
2020-06-06 11:38:13 -0600

    Render json

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,9 +1,10 @@
 class Api::V1::ForecastController < ApplicationController
 
-def show
-  location = params["location"]
-  weather_info = SearchResults.new.weather(location)
-end
+  def show
+    location = params["location"]
+    weather_info = SearchResults.new.weather(location)
+    render json: WeatherSerializer.new(weather_info).serialized_json
+  end
 
 
 end
