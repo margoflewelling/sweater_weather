0254e9dc7d9947ac7a6e85026595c44482cdf8b7
Margo Flewelling <flewelling.margo@gmail.com>
2020-06-06 16:58:55 -0600

    Add sad path for invalid login

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -6,6 +6,8 @@ class Api::V1::SessionsController < ApplicationController
     if user.authenticate(user_info[:password])
       session[:user_id] = user.id
       render json: UserSerializer.new(user).serialized_json
+    else
+      render json: {status: "error", code: 400, message: "Incorrect password"}
     end
   end
 
--- spec/requests/api/v1/login_spec.rb
+++ spec/requests/api/v1/login_spec.rb
@@ -15,4 +15,20 @@ describe "Logging in" do
     expect(user["data"]["attributes"].has_key?("email")).to eq(true)
     expect(user["data"]["attributes"].has_key?("api_key")).to eq(true)
   end
+
+  it "user can not login without correct credentials" do
+    User.create!(email: 'margo@example.com', password: "123", password_confirmation: "123")
+    user_info = {
+                "email": "margo@example.com",
+                "password": "12",
+                }
+
+    post '/api/v1/sessions', params: user_info.to_json, as: :json
+    expect(response).to be_successful
+    error = JSON.parse(response.body)
+    expect(error["status"]).to eq("error")
+    expect(error["code"]).to eq(400)
+    expect(error["message"]).to eq("Incorrect password")
+  end
+
 end
