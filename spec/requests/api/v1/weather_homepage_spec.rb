525ba3e0b29fff4518982d09f877f50c0808e450
Margo Flewelling <flewelling.margo@gmail.com>
2020-06-05 23:10:15 -0600

    Updsate test for background image

--- spec/requests/api/v1/weather_homepage_spec.rb
+++ spec/requests/api/v1/weather_homepage_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 describe "Weather information" do
-  xit "can send image of city weather" do
+  it "can send image of city weather" do
     get '/api/v1/background?location=denver, co'
     expect(response).to be_successful
     image = JSON.parse(response.body)
+    expect(image.is_a? Hash).to eq(true)
+    expect(image["data"]["attributes"].has_key?("image")).to eq(true)
+    expect(image["data"]["attributes"].has_key?("city")).to eq(true)
   end
 
   it "can get weather data for a location" do
     get '/api/v1/forecast?location=denver, co'
     expect(response).to be_successful
-    
+    # weather = JSON.parse(response.body)
+    # require "pry"; binding.pry
 
   end
 end
