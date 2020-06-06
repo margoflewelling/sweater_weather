9f16ace1bf4a0a8bde6ee504609f5dd3ef8713ce
Margo Flewelling <flewelling.margo@gmail.com>
2020-06-05 23:08:28 -0600

    Remove pry

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -3,7 +3,6 @@ class Api::V1::ForecastController < ApplicationController
 def show
   location = params["location"]
   weather_info = SearchResults.new.weather(location)
-  require "pry"; binding.pry
 end
 
 
