0822c84dff09bd31b387e912712719a05d4bf19b
Margo Flewelling <flewelling.margo@gmail.com>
2020-06-08 10:25:30 -0600

    Refactor restaurant service

--- app/controllers/api/v1/foodie_controller.rb
+++ app/controllers/api/v1/foodie_controller.rb
@@ -3,6 +3,6 @@ class Api::V1::FoodieController < ApplicationController
   def show
     foodie = SearchResults.new.food(params["start"], params["end"], params["search"])
     render json: FoodieSerializer.new(foodie).serialized_json
-      end
+  end
 
 end
--- app/services/restaurant_service.rb
+++ app/services/restaurant_service.rb
@@ -1,17 +1,23 @@
 class RestaurantService
 
   def restaurant(search, geocoordinates)
-    food = Faraday.get('https://developers.zomato.com/api/v2.1/search') do |f|
-        f.headers['user-key'] = ENV['FOOD_KEY']
-        f.params['lat'] = geocoordinates[:lat]
-        f.params['lon'] = geocoordinates[:lng]
-        f.params['q'] = search
-        f.params['count'] = 1
-      end
-    json = JSON.parse(food.body, symbolize_names: true)
-    name = json[:restaurants].first[:restaurant][:name]
-    address = json[:restaurants].first[:restaurant][:location][:address]
-    restaurant = {name: name, address: address}
+    json = JSON.parse(conn(search, geocoordinates).body, symbolize_names: true)
+    { name: json[:restaurants].first[:restaurant][:name],
+      address: json[:restaurants].first[:restaurant][:location][:address] }
   end
+
+private
+
+  def conn(search, geocoordinates)
+    uri = 'https://developers.zomato.com/api/v2.1/search'
+    Faraday.get(uri) do |f|
+      f.headers['user-key'] = ENV['FOOD_KEY']
+      f.params['lat'] = geocoordinates[:lat]
+      f.params['lon'] = geocoordinates[:lng]
+      f.params['q'] = search
+      f.params['count'] = 1
+    end
+  end
+
 
 end
