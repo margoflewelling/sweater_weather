039feca57ba38d49852274ae72c6e55fbd481510
Margo Flewelling <flewelling.margo@gmail.com>
2020-06-07 20:26:57 -0600

    update status as created

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::UsersController < ApplicationController
     user_info = JSON.parse(request.body.read, symbolize_names: true)
     user = User.create(user_info)
     if user.save
-      render json: UserSerializer.new(user).serialized_json, status: :created
+      render :status => :created, json: UserSerializer.new(user).serialized_json
     else
       message = user.errors.full_messages.to_sentence
       render :status => "400", json: {message: message}
