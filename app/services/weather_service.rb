72a3a13cb06eaf09a7ea5d2cf9bc2020280b644f
Margo Flewelling <flewelling.margo@gmail.com>
2020-06-08 11:58:22 -0600

    Last commit

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,14 +1,18 @@
 class WeatherService
 
   def weather(coordinates)
-    weather = Faraday.get("https://api.openweathermap.org/data/2.5/onecall") do |f|
+    JSON.parse(conn(coordinates).body, symbolize_names: true)
+  end
+
+private
+
+  def conn(coordinates)
+    Faraday.get("https://api.openweathermap.org/data/2.5/onecall") do |f|
       f.params['lat'] = coordinates[:lat]
       f.params['lon'] = coordinates[:lng]
       f.params['exclude'] = 'minutely'
       f.params['appid'] = ENV['WEATHER_KEY']
       f.params['units'] = 'imperial'
     end
-    JSON.parse(weather.body, symbolize_names: true)
   end
-    
 end
